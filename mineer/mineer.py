1b34265f8befd39aa38ce42e1ce3607c33aef96c
michaelsilverstein <michael.silverstein4@gmail.com>
2021-07-19 13:13:19 -0400

    closes #10. closes #2

--- mineer/mineer.py
+++ mineer/mineer.py
@@ -5,6 +5,39 @@ AUTHOR: MICHAEL SILVERSTEIN
 EMAIL: michael.silverstein4@gmail.com
 """
 import numpy as np
+from numba import jit
+
+@jit(nopython=True)
+def eerspace(ee, mal):
+    """
+    Compute expected error rate space where each row is starting position and each column is an ending position
+
+    For each starting position, calculate the expected error rate of every subsequence of the minimum acceptable length
+    """
+    # Initialize matrix
+    n_pos = len(ee)
+    eer_space = np.empty((n_pos, n_pos))
+    eer_space[:] = np.nan
+    # Compute the cumulative sum of the expected errors
+    cumsum_ee = np.cumsum(ee)
+    # Get normalizing vector to compute EER at each position
+    norm = np.arange(n_pos) + 1
+    # Initialize first start
+    eer_space[0, mal:] = (cumsum_ee / norm)[mal:]
+
+    # For each starting position
+    for start in range(1, n_pos - mal):
+        # Get cumulative sum from this start position
+        cumsum_ee_start = cumsum_ee[start:]
+
+        # Calculate the expected errors from this start position
+        eers = (cumsum_ee_start - cumsum_ee[start - 1]) / norm[:-start]
+        # Only take the expected errors starting at the minimum acceptable length
+        eers_mal = eers[mal:]
+        # Update the search space
+        eer_space[start, (start + mal) :] = eers_mal
+    return eer_space
+
 
 def minEER(ee, mal=100, mae=1e-3):
     """
@@ -19,20 +52,8 @@ def minEER(ee, mal=100, mae=1e-3):
     """
     # Ensure np.array()
     ee = np.array(ee)
-    # Initialize matrix
-    n_pos = len(ee)
-    eer_space = np.empty((n_pos, n_pos))
-    eer_space[:] = np.nan
-    # For each starting position
-    for start in range(n_pos - mal):
-        # Get expected error profile from this start
-        ee_start = ee[start:]
-        # Calculate the expected errors starting at this position
-        eers = ee_start.cumsum() / (np.arange(len(ee_start)) + 1)
-        # Only take the expected errors starting at the minimum acceptable length
-        eers_mal = eers[mal:]
-        # Update the search space
-        eer_space[start, (start + mal) :] = eers_mal
+    # Calculate expected error space
+    eer_space = eerspace(ee, mal)
 
     """Find max length sequence below threshold"""
     # Filter out nans to avoid warning about trying to evaluate np.nan<thresh
@@ -47,17 +68,6 @@ def minEER(ee, mal=100, mae=1e-3):
         # Find longest subsequence
         max_idx = np.argmax(end_ids - start_ids)
         start_pos, end_pos = start_ids[max_idx], end_ids[max_idx]
+        # Add one to end pos to be inclusive when indexing
+        end_pos += 1
     return start_pos, end_pos 
-
-# if __name__ == '__main__':
-#     parser = ArgumentParser(formatter_class=RawTextHelpFormatter)
-#     parser.add_argument('-i', help='Input, type specified with -t', type=str, required=True)
-#     parser.add_argument('-t', help='Input type:\n'
-#                                    '\tfile: If -i is a path to a single file'
-#                                    '\tdir: If -i is a path to a directory only containing files to trim'
-#                                    '\tlist: If -i is a path to a list of paths of files to trim',
-#                         choice=['file', 'dir', 'list'], type=str, required=True)
-#     # TODO: allow per-file specificiation (could have input be CSV with filepath and format)
-#     parser.add_argument('-f', help='File format of file(s) passed in -i', required=True)
-#     parser.add_argument('-o', help='Output directory\n'
-#                                    'Output files will be named as /output_dir/{original_name}_trimmed.{extension}')
