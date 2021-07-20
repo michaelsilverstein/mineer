64d93d8a164cf47d83291d7536904201e38499cc
michaelsilverstein <michael.silverstein4@gmail.com>
2021-07-20 13:06:34 -0400

    single end working. closes #19

--- test/test_utils.py
+++ test/test_utils.py
@@ -5,7 +5,7 @@ from mineer.utils import Project
 from unittest import TestCase
 from copy import copy
 
-class TestProject(TestCase):
+class TestProjectPaired(TestCase):
     def setUp(self):
         samples = ['a', 'b']
         suffixes = ['_1.fastq', '_2.fastq']
@@ -20,22 +20,44 @@ class TestProject(TestCase):
             aggmethod = 'mean',
             outdir = 'test'
         )
-    
+        self.project = Project(**self.kwargs)
+
     def test_project_ingest(self):
-        project = Project(**self.kwargs)
         for arg, val in self.kwargs.items():
-            project_val = getattr(project, arg)
+            project_val = getattr(self.project, arg)
             self.assertEqual(val, project_val)
     
     def test_ignore_bad_files(self):
         bad_paths = ['bad', 'paths']
         expected = copy(self.kwargs['filepaths'])
         self.kwargs['filepaths'].extend(bad_paths)
-        project = Project(**self.kwargs)
-        self.assertListEqual(project.filepaths, expected)
+        self.assertListEqual(self.project.filepaths, expected)
 
+class TestProjectSingle(TestCase):
+    def setUp(self):
+        filepaths = ['a.fastq', 'b.fastq']
+        self.kwargs = dict(
+        filepaths = filepaths,
+        fwd_format = '.fastq',
+        nreads = 10,
+        mal = 20,
+        mae = 1,
+        aggmethod = 'mean',
+        outdir = 'test'
+    )
+
+    def test_project_ingest(self):
+        project = Project(**self.kwargs)
+        for arg, val in self.kwargs.items():
+            project_val = getattr(project, arg)
+            self.assertEqual(val, project_val)
+    
     def test_not_paired(self):
-        self.kwargs.pop('rev_format')
         project = Project(**self.kwargs)
         self.assertFalse(project.paired)
 
+    def test_only_forward(self):
+        bad_paths = ['a_2.fastq', 'b_2.fastq']
+        expected = copy(self.kwargs['filepaths'])
+        self.kwargs['filepaths'].extend(bad_paths)
+        self.assertListEqual(self.project.filepaths, expected)
