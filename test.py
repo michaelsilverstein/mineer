72d9467e884d3bb51dbda483ea0b771308a1c360
michaelsilverstein <michael.silverstein4@gmail.com>
2021-08-02 15:38:05 -0400

    multiprocessing read2record doesn't work

--- test.py
+++ test.py
@@ -1,34 +1,60 @@
 import multiprocessing as mp
-from mineer.utils import File
+from mineer.utils import File, RawFastqGenerator
 from mineer.mineer import minEER
-import random
+from Bio import SeqIO
+from io import StringIO
 from time import time
 
 def mpRunMineer(read):
     trimpos_mineer = minEER(read.untrimmed.ee, read.mal, read.mae)
     return trimpos_mineer
 
+def read2record(read):
+    with StringIO(read) as s:
+        return SeqIO.read(s, 'fastq')
+
+def chunkread2record(chunk):
+    return [read2record(read) for read in chunk]
 
 if __name__ == '__main__':
-    file = File('test_files/SRR9660307_1.fastq', 'f', 100, 1e-2)
-    reads = file.reads
-    for r in reads:
-        r.untrimmed
+    # file = File('test_files/SRR9660307_1.fastq', 'f', 100, 1e-2)
+    # reads = file.reads
+    # for r in reads:
+    #     r.untrimmed
+    reads = RawFastqGenerator('test_files/SRR9218919_1.fastq')
+    # print(len(reads))
 
     start = time()
-    for read in reads:
-        read.runMineer()
+    records = [read2record(read) for read in reads]
     print('Single thread')
+    print(records[:3])
+    print(len(records))
     print(f'{time() - start:.3e} seconds')
-    print([r.trimpos_mineer for r in reads[:10]])
 
+    print()
+    reads = RawFastqGenerator('test_files/SRR9218919_1.fastq')
     start = time()
     pool = mp.Pool()
-    print(pool)
-    jobs = pool.map_async(mpRunMineer, reads, 1000).get()
-    print('Multiprocessed')
+    records = pool.map_async(read2record, reads, 10000).get()
+    print('Multithreaded')
+    print(len(records))
+    print(records[:3])
     print(f'{time() - start:.3e} seconds')
-    print(jobs[:10])
+
+    # start = time()
+    # for read in reads:
+    #     read.runMineer()
+    # print('Single thread')
+    # print(f'{time() - start:.3e} seconds')
+    # print([r.trimpos_mineer for r in reads[:10]])
+
+    # start = time()
+    # pool = mp.Pool()
+    # print(pool)
+    # jobs = pool.map_async(mpRunMineer, reads, 1000).get()
+    # print('Multiprocessed')
+    # print(f'{time() - start:.3e} seconds')
+    # print(jobs[:10])
 
     # nreads = 5000
     # random.shuffle(reads)
